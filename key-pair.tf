6a2998e06dc9ac137f890c202d377793466804cc
bhrigushandilya <65305623+bhrigushandilya@users.noreply.github.com>
2020-06-15 11:35:27 +0530

    second

--- key-pair.tf
+++ key-pair.tf
@@ -1,5 +1,20 @@
 // Creating key pair
-resource "aws_key_pair" "key"{
+resource "tls_private_key" "key1"{
+	algorithm = "RSA"
+}
+
+resource "aws_key_pair" "generate_key"{
 	key_name = "key1"
-	public_key = file("C:/Users/Bhrigu/.ssh/id_rsa.pub")
+	public_key = tls_private_key.key1.public_key_openssh
+	depends_on = [
+		tls_private_key.key1
+	]
+}
+
+resource "local_file" "key_text"{
+	content = tls_private_key.key1.private_key_pem
+	filename = "key1.pem"
+	depends_on = [
+		tls_private_key.key1
+	]
 }
